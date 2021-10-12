83f0a5d700f1e9f6ee624f007b6348d66181255b
unknown <duvandsm25@gmail.com>
2021-10-12 16:25:57 -0500

    commit sergio

--- backend/routes/tareas.js
+++ backend/routes/tareas.js
@@ -1,25 +1,49 @@
 const express= require('express');
+const jwt = require('jsonwebtoken');
+//-------------------
+const bcrypt = require('bcrypt')
+//-------------------
 const rutas = express.Router();
 
 const usuario = require('../models/usuario')
 
 //Registrar usuario
 
-rutas.post('/registrar-usuario', async (req,res) =>{
-    const body= req.body;
+rutas.post('/registrar-usuario', async (req,res, next) =>{
+    const userExist= await usuario.findOne({usuario: req.body.usuario});
+    if(userExist){
+        return res.status(400).json({error: 'El usuario ya está registrado'})
+    }
+    const encrypt= await bcrypt.genSalt(10);
+    const password= await bcrypt.hash(req.body.password, encrypt);
 
+    const user= new usuario({
+        usuario: req.body.usuario,
+        password: password
+    });
+
+    usuario.create(user, (error, data)=>{
+        if(error){
+            return next(error);
+        }else{
+            console.log(data);
+            res.json(data);
+        }
+    });
+    
+});
+
+//Logear
+
+rutas.get('/login', async (req,res)=> {
     try{
-
-        const userDB= await usuario.create(body);
-        res.status(200).json(userDB)
-
-    }catch(error){
-
-        return res.status(500).json({
+        const user= await usuario.find();
+        res.json(user);
+    } catch {
+        return res.status(400).json({
             mensaje: 'Ocurrio un error',
-            err
+            error
         })
-
     }
 })
 
--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Hello team")
+/* import axios from 'axios'
+
+let apiURL= 'http://localhost:3000/api';
+axios.get(apiURL).then(() => {
+    console.log('conectado existosamente a la api');
+}).catch((error)=>{
+    console.log('ocurrio un error ÑOOOO')
+}) */
+fetch('http://localhost:3000/api/login', {
+    method: 'GET'
+})
+.then((response) => {
+    console.log('conectado a la api')
+})
